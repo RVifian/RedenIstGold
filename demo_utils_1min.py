e389b26db8f740fbd7d1557875cf9f184ecd0672
Dejan <deejan.t@gmail.com>
2020-12-17 22:09:51 +0100

    deleted unnecessary code

--- demo_utils_1min.py
+++ demo_utils_1min.py
@@ -45,85 +45,6 @@ def play_wav(wav, blocking=True):
     except Exception as e:
         print("Failed to play audio: %s" % repr(e))
 
-
-def plot_similarity_matrix(matrix, labels_a=None, labels_b=None, ax: plt.Axes=None, title=""):
-    if ax is None:
-        _, ax = plt.subplots()
-    fig = plt.gcf()
-        
-    img = ax.matshow(matrix, extent=(-0.5, matrix.shape[0] - 0.5, 
-                                     -0.5, matrix.shape[1] - 0.5))
-
-    ax.xaxis.set_ticks_position("bottom")
-    if labels_a is not None:
-        ax.set_xticks(range(len(labels_a)))
-        ax.set_xticklabels(labels_a, rotation=90)
-    if labels_b is not None:
-        ax.set_yticks(range(len(labels_b)))
-        ax.set_yticklabels(labels_b[::-1])  # Upper origin -> reverse y axis
-    ax.set_title(title)
-
-    cax = make_axes_locatable(ax).append_axes("right", size="5%", pad=0.15)
-    fig.colorbar(img, cax=cax, ticks=np.linspace(0.4, 1, 7))
-    img.set_clim(0.4, 1)
-    img.set_cmap("inferno")
-    
-    return ax
-    
-    
-def plot_histograms(all_samples, ax=None, names=None, title=""):
-    """
-    Plots (possibly) overlapping histograms and their median 
-    """
-    if ax is None:
-        _, ax = plt.subplots()
-    
-    for samples, color, name in zip(all_samples, _default_colors, names):
-        ax.hist(samples, density=True, color=color + "80", label=name)
-    ax.legend()
-    ax.set_xlim(0.35, 1)
-    ax.set_yticks([])
-    ax.set_title(title)
-        
-    ylim = ax.get_ylim()
-    ax.set_ylim(*ylim)      # Yeah, I know
-    for samples, color in zip(all_samples, _default_colors):
-        median = np.median(samples)
-        ax.vlines(median, *ylim, color, "dashed")
-        ax.text(median, ylim[1] * 0.15, "median", rotation=270, color=color)
-    
-    return ax
-
-
-def plot_projections(embeds, speakers, ax=None, colors=None, markers=None, legend=True, 
-                     title="", **kwargs):
-    if ax is None:
-        _, ax = plt.subplots(figsize=(6, 6))
-        
-    # Compute the 2D projections. You could also project to another number of dimensions (e.g. 
-    # for a 3D plot) or use a different different dimensionality reduction like PCA or TSNE.
-    reducer = UMAP(**kwargs)
-    projs = reducer.fit_transform(embeds)
-    
-    # Draw the projections
-    speakers = np.array(speakers)
-    colors = colors or _my_colors
-    for i, speaker in enumerate(np.unique(speakers)):
-        speaker_projs = projs[speakers == speaker]
-        marker = "o" if markers is None else markers[i]
-        label = speaker if legend else None
-        ax.scatter(*speaker_projs.T, c=[colors[i]], marker=marker, label=label)
-
-    if legend:
-        ax.legend(title="Speakers", ncol=2)
-    ax.set_title(title)
-    ax.set_xticks([])
-    ax.set_yticks([])
-    ax.set_aspect("equal")
-    
-    return projs
-    
-
 def interactive_diarization(similarity_dict, wav, wav_splits, x_crop=5, show_time=True):    
     fig, (ax, pie) = plt.subplots(1, 2)
     lines = [ax.plot([], [], label=name)[0] for name in similarity_dict.keys()]
@@ -237,22 +158,3 @@ def interactive_diarization(similarity_dict, wav, wav_splits, x_crop=5, show_tim
     global sandroTime
     print(karinTime * timeFrameMultiplier)
     print(sandroTime * timeFrameMultiplier)
-
-
-def plot_embedding_as_heatmap(embed, ax=None, title="", shape=None, color_range=(0, 0.30)):
-    if ax is None:
-        _, ax = plt.subplots()
-    
-    if shape is None:
-        height = int(np.sqrt(len(embed)))
-        shape = (height, -1)
-    embed = embed.reshape(shape)
-    
-    cmap = cm.get_cmap()
-    mappable = ax.imshow(embed, cmap=cmap)
-    cbar = plt.colorbar(mappable, ax=ax, fraction=0.046, pad=0.04)
-    cbar.set_clim(*color_range)
-    
-    ax.set_xticks([]), ax.set_yticks([])
-    ax.set_title(title)
-
